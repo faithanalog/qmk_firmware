021a101bf2b9967a0cab5f1dc86bf6aa67d8a1a4
Galen Alderson <unknownloner.online@gmail.com>
2017-05-08 00:24:09 -0400

    Add a little more debounce delay for s60_x

--- keyboards/s60_x/config.h
+++ keyboards/s60_x/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCING_DELAY 7
 
 /* key combination for command */
 #define IS_COMMAND() ( \
